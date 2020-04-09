313d8bf96f0dd2145e333b4c290137a10152092e
merrycatch22 <tarn.nicholas@gmail.com>
2020-04-08 23:33:03 -0400

    piazza test.sh fix

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 SHARE=$(pwd)
 IMAGE=$1
-NAME=$(sudo docker run -d -v ${SHARE}:/host/Users -it ${IMAGE} /bin/bash)
+NAME=$(sudo docker run -d -v "${SHARE}:/host/Users" -it ${IMAGE} /bin/bash)
 echo '****************'
 sudo docker exec -it $NAME ./ask /host/Users/data/set1/a1.txt 3
 echo '****************'
