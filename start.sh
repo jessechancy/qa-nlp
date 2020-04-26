fa0ce30e129cd76fc925bdd3ac8d6f6a2cc5c38c
merrycatch22 <tarn.nicholas@gmail.com>
2020-04-26 01:40:17 -0400

    3g again

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-nohup java -mx4g -cp "CoreNLP/*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer -preload tokenize,ssplit,pos,lemma,ner,parse,depparse -status_port 9000 -port 9000 -timeout 15000 &
+nohup java -mx3g -cp "CoreNLP/*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer -preload tokenize,ssplit,pos,lemma,ner,parse,depparse -status_port 9000 -port 9000 -timeout 15000 &
 exec "$@"
